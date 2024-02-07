a4a2391c26e8dde8842c43189f3d9b1cab648b04
Kirill Bugaev <lordnight40@gmail.com>
2024-02-07 03:31:14 +0300

    fixes

--- ItPro.Data/StoredProcedures/GetHourlyAverageSumByStatus.sql
+++ ItPro.Data/StoredProcedures/GetHourlyAverageSumByStatus.sql
@@ -1,31 +1,29 @@
 USE [ItPro]
 GO
 IF EXISTS(
-    SELECT
-        [type_desc],
-        [type]
-    FROM [sys].[procedures] WITH(NOLOCK)
-    WHERE [name] = 'GetHourlyAverageSumByStatus'
-        AND [type] = 'p'
+	SELECT
+		[type_desc],
+		[type]
+	FROM [sys].[procedures] WITH(NOLOCK)
+	WHERE [name] = 'GetHourlyAverageSumByStatus'
+	  AND [type] = 'p'
 ) DROP PROCEDURE [dbo].[GetHourlyAverageSumByStatus]
 GO
 CREATE PROCEDURE [dbo].[GetHourlyAverageSumByStatus]
-	@status varchar
+@status nvarchar(max)
 AS
 BEGIN
-	-- получаем часы
-    WITH [Intervals] AS (
-	SELECT 
-		CAST(DATEADD(HOUR, [s].[value], '00:00') AS TIME) AS [Hour]
-	FROM GENERATE_SERIES(0, 23, 1) AS [s]
-	)
 	SELECT
 		[i].[Hour]									AS [Start],
 		CAST(AVG([o].[Amount]) AS DECIMAL(10,2))	AS [Average]
-	FROM [Orders] AS [o]
-	INNER JOIN [Intervals] AS [i]
-		ON CAST([o].[CreatedAt] AS TIME) BETWEEN [i].[Hour] AND DATEADD(MINUTE, 59, [i].[Hour])
-	WHERE [o].[Status] = @status
+	FROM [Orders] AS [o] WITH (NOLOCK)
+			 CROSS APPLY (
+		SELECT
+			CAST(DATEADD(HOUR, [s].[value], '00:00') AS TIME) AS [Hour]
+		FROM GENERATE_SERIES(0, 23, 1) AS [s]
+	) AS [i] --ON CAST([o].[CreatedAt] AS TIME) BETWEEN [i].[Hour] AND DATEADD(MINUTE, 59, [i].[Hour])
+	WHERE LOWER([o].[Status]) = LOWER(@status)
+	  AND CAST([o].[CreatedAt] AS TIME) BETWEEN [i].[Hour] AND DATEADD(MINUTE, 59, [i].[Hour])
 	GROUP BY [i].[Hour]
 	ORDER BY [Start] ASC;
 END
